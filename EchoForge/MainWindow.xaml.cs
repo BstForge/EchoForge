6b239d204f20dc014f95cfcd8a0e7563f84a697a
BstForge <archimage2869@gmail.com>
2025-07-27 21:26:20 -0500

    0.1.4 - Ribbon and View Animations - Added animated retraction and expansion of ribbon and content panes - Seamless transition when switching views via left menu - All animations implemented using native WPF Storyboards

--- EchoForge/MainWindow.xaml.cs
+++ EchoForge/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Threading.Tasks;
 using EchoForge.Panes;
 using EchoForge.Menus;
 
@@ -32,35 +34,76 @@ namespace EchoForge
             MainRibbon.Content = ribbon;
         }
 
+        private Storyboard GetStoryboard(string key, DependencyObject target)
+        {
+            var sb = (Storyboard)FindResource(key);
+            sb = sb.Clone();
+            foreach (var timeline in sb.Children)
+            {
+                Storyboard.SetTarget(timeline, target);
+            }
+            return sb;
+        }
+
+        private Task BeginStoryboardAsync(Storyboard storyboard)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            if (storyboard == null)
+            {
+                tcs.SetResult(true);
+                return tcs.Task;
+            }
+            void End(object? s, EventArgs e)
+            {
+                storyboard.Completed -= End;
+                tcs.SetResult(true);
+            }
+            storyboard.Completed += End;
+            storyboard.Begin();
+            return tcs.Task;
+        }
+
+        private async Task TransitionAsync(UserControl view, UserControl ribbon)
+        {
+            var contentRetract = GetStoryboard("ContentRetractStoryboard", MainContentBorder);
+            var ribbonRetract = GetStoryboard("RibbonRetractStoryboard", RibbonContainer);
+            var ribbonExpand = GetStoryboard("RibbonExpandStoryboard", RibbonContainer);
+            var contentExpand = GetStoryboard("ContentExpandStoryboard", MainContentBorder);
+
+            await BeginStoryboardAsync(contentRetract);
+            await BeginStoryboardAsync(ribbonRetract);
+
+            LoadView(view);
+            SetRibbon(ribbon);
+
+            await BeginStoryboardAsync(ribbonExpand);
+            await BeginStoryboardAsync(contentExpand);
+        }
+
         // Button click handlers
-        private void Input_Click(object sender, RoutedEventArgs e)
+        private async void Input_Click(object sender, RoutedEventArgs e)
         {
-            LoadView(new InputView());
-            SetRibbon(_inputRibbon);
+            await TransitionAsync(new InputView(), _inputRibbon);
         }
 
-        private void Parsed_Click(object sender, RoutedEventArgs e)
+        private async void Parsed_Click(object sender, RoutedEventArgs e)
         {
-            LoadView(new ParsedView());
-            SetRibbon(_parsedRibbon);
+            await TransitionAsync(new ParsedView(), _parsedRibbon);
         }
 
-        private void Generate_Click(object sender, RoutedEventArgs e)
+        private async void Generate_Click(object sender, RoutedEventArgs e)
         {
-            LoadView(new GenerateView());
-            SetRibbon(_generateRibbon);
+            await TransitionAsync(new GenerateView(), _generateRibbon);
         }
 
-        private void Audio_Click(object sender, RoutedEventArgs e)
+        private async void Audio_Click(object sender, RoutedEventArgs e)
         {
-            LoadView(new AudioFilesView());
-            SetRibbon(_audioRibbon);
+            await TransitionAsync(new AudioFilesView(), _audioRibbon);
         }
 
-        private void Voice_Click(object sender, RoutedEventArgs e)
+        private async void Voice_Click(object sender, RoutedEventArgs e)
         {
-            LoadView(new VoiceOptionsView());
-            SetRibbon(_voiceRibbon);
+            await TransitionAsync(new VoiceOptionsView(), _voiceRibbon);
         }
     }
 }
